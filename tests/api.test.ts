1a7a7c5a9c250fad80a4b7137d20f2c5a7a80daa
Bobby Westberg <bellfalasch@gmail.com>
2023-11-15 15:19:53 +0100

    test: reset mocks on each pass, count for every test

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -1,46 +1,55 @@
-import { describe, test, expect, mock, spyOn } from "bun:test";
+import { describe, test, expect, mock, spyOn, beforeEach } from "bun:test";
 
 import { imagePathFromLocationName } from '../src/lib/utils';
 
 const pathFromLocation = mock((locationName, teamNr?) => imagePathFromLocationName(locationName, teamNr || undefined));
 //const spy = spyOn(imagePathFromLocationName);
 
+beforeEach(
+  () => { pathFromLocation.mockClear(); }
+)
+
 describe('getPathNameFromLocationName', () => {
   test('should return the correct IMG path for New York Rangers', () => {
     const locationName = 'New York';
     const pathName = pathFromLocation(locationName, "3");
     expect(pathName).toEqual('newyorkrangers');
-    expect(pathFromLocation).toHaveBeenCalledTimes(1)
+    expect(pathFromLocation).toHaveBeenCalledTimes(1);
   });
   test('should return the correct IMG path for Los Angeles', () => {
     const locationName = 'Los Angeles';
     const pathName = pathFromLocation(locationName);
     expect(pathName).toEqual('losangeles');
+    expect(pathFromLocation).toHaveBeenCalledTimes(1);
   });
   test('should return the correct IMG path for New York Islanders', () => {
     const locationName = 'New York';
     const pathName = pathFromLocation(locationName, "2");
     expect(pathName).toEqual('newyorkislanders');
+    expect(pathFromLocation).toHaveBeenCalledTimes(1);
   });
   test('should return the correct IMG path for New Jersey', () => {
     const locationName = 'New Jersey';
     const pathName = pathFromLocation(locationName);
     expect(pathName).toEqual('newjersey');
-    //expect(pathFromLocation).toHaveBeenCalledTimes(1);
+    expect(pathFromLocation).toHaveBeenCalledTimes(1);
   });
   test('should return the correct IMG path for San José', () => {
     const locationName = 'San José';
     const pathName = pathFromLocation(locationName);
     expect(pathName).toEqual('sanjose');
+    expect(pathFromLocation).toHaveBeenCalledTimes(1);
   });
   test('should return the correct IMG path for Washington', () => {
     const locationName = 'Washington';
     const pathName = pathFromLocation(locationName);
     expect(pathName).toEqual('washington');
+    expect(pathFromLocation).toHaveBeenCalledTimes(1);
   });
   test('should return the correct IMG path for St. Louis', () => {
     const locationName = 'St. Louis';
     const pathName = pathFromLocation(locationName);
     expect(pathName).toEqual('stlouis');
+    expect(pathFromLocation).toHaveBeenCalledTimes(1);
   });
 });
